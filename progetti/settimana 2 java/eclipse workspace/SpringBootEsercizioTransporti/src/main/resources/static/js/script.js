af265bff805611b667770c8ce7ad91d6b85935d8
salvo720 <salvocelano91@gmail.com>
2023-03-15 21:00:47 +0100

    aggiunti nel file index.html , i file main.css e script.js correttamente funzionanti da implementare un ajax per il recuperdo degli autisti e da creare un inserimento

--- progetti/settimana 2 java/eclipse workspace/SpringBootEsercizioTransporti/src/main/resources/static/js/script.js
+++ progetti/settimana 2 java/eclipse workspace/SpringBootEsercizioTransporti/src/main/resources/static/js/script.js
@@ -1,3 +1,25 @@
-debug=true
-server.port=8081
-spring.thymeleaf.cache=false
+document.addEventListener("DOMContentLoaded", function (event) {
+    //do work
+    console.log("js ready");
+    
+    
+    let tbody = document.querySelector("tbody");
+		let tr = document.createElement("tr");
+		let td = document.createElement("td");
+		$.ajax({
+			url : "/api/autisti",
+			contentType : 'json'
+		}).done(function(data) {
+			// inserire tutti i dati con un for 
+			let testotd;
+			for(let index=0;index<data.lenght;index++){
+			testotd[index]=document.createElement("td");
+			testotd= td+index;
+			testotd.textContet = data.id 
+			tr.appendChild(testotd);
+			}
+			console.log(data)
+			// $(this).addClass("done");
+        })
+    
+});
