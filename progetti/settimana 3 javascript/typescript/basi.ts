efb69e56ce2182e1494e28eb8ad7419538d99bf2
salvo720 <salvoccelano91gmail.com>
2023-03-17 09:12:19 +0100

    modifiche sulla cartella javascript>typescript

--- progetti/settimana 3 javascript/typescript/basi.ts
+++ progetti/settimana 3 javascript/typescript/basi.ts
@@ -5,3 +5,23 @@ const stringa:string = 'ciao';
 
 console.log({stringa});
 
+// definire un tipo stringente 
+let stingastretta : "ciao" | "hola" = "ciao";
+
+stingastretta = "hola";
+// stingastretta = "holsa"; // segna un errore perche non posso assegnare un valore al di fuori di quello definito "ciao" od "hola"
+
+
+type Greeting = string | number ; // creo un tipo personalizzato 
+
+let saluto : Greeting;
+saluto = "ciao";
+saluto = 9;
+saluto = "prova";
+saluto = "asdasd";
+
+type Coder = {name:string , skills:string[]}
+type Coder2 = {name:string , skills:Array<string>}
+
+type Skills = string | number ; // creo un tipo personalizzato 
+type Coder3 = {name:string , skills:Array<Skills>}
