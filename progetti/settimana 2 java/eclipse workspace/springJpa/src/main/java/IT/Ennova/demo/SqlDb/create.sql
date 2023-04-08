bfa07172206a8102c6b93b5ff95b13bbfe85e714
salvo720 <salvocelano91@gmail.com>
2023-04-08 20:25:10 +0200

    chore(springJpa) : spostata la cartella Dbprogetto da springJpa>src>main>ja>IT>Ennova>Demo>Model> a progetti>settimana 2 java>eclipse workspace

--- progetti/settimana 2 java/eclipse workspace/springJpa/src/main/java/IT/Ennova/demo/SqlDb/create.sql
+++ progetti/settimana 2 java/eclipse workspace/springJpa/src/main/java/IT/Ennova/demo/SqlDb/create.sql
@@ -3,3 +3,11 @@ create table articoli (id bigint not null, data_rilascio date, descr varchar(100
 create table fornitori (id bigserial not null, cognome varchar(100) not null, foto varchar(255), nome varchar(255), primary key (id));
 create table mezzo (id serial not null, modello Varchar(50) not null, stato boolean not null, tipo Varchar(50) not null, primary key (id));
 alter table if exists articoli add constraint FK8argq7l01roldr171th664h4w foreign key (id_fornitore) references fornitori;
+create table articoli (id bigint not null, data_rilascio date, descr varchar(100) not null, prezzo float4 not null, id_fornitore bigint, primary key (id));
+create table fornitori (id bigserial not null, cognome varchar(100) not null, foto varchar(255), nome varchar(255), primary key (id));
+create table mezzo (id serial not null, modello Varchar(50) not null, stato boolean not null, tipo Varchar(50) not null, primary key (id));
+alter table if exists articoli add constraint FK8argq7l01roldr171th664h4w foreign key (id_fornitore) references fornitori;
+create table articoli (id bigint not null, data_rilascio date, descr varchar(100) not null, prezzo float4 not null, id_fornitore bigint, primary key (id));
+create table fornitori (id bigserial not null, cognome varchar(100) not null, foto varchar(255), nome varchar(255), primary key (id));
+create table mezzo (id serial not null, modello Varchar(50) not null, stato boolean not null, tipo Varchar(50) not null, primary key (id));
+alter table if exists articoli add constraint FK8argq7l01roldr171th664h4w foreign key (id_fornitore) references fornitori;
