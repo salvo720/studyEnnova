58102ddc88ab08ca70471b2494b3baa3ddd9f52f
salvo720 <salvocelano91@gmail.com>
2023-05-02 21:24:11 +0200

    feat(angular) : regex completati i vedeo sulle regex e scritta la prima regex per validare l'email nel componente regex-js

--- progetti/settimana 3 javascript/typescritAngular/angular/src/app/components/regex-js/regex-js.component.ts
+++ progetti/settimana 3 javascript/typescritAngular/angular/src/app/components/regex-js/regex-js.component.ts
@@ -24,7 +24,7 @@ export class RegexJsComponent implements OnInit {
     this.formUser = this.fb.group({
       nome: ['nome', []],
       cognome: ['cognome', []],
-      email: ['email', []],
+      email: ['email@gmail.com', []],
       password: ['password', []],
       descrizione: ['descrizione', []],
     }, [Validators.maxLength(10), Validators.required]);
@@ -38,6 +38,8 @@ export class RegexJsComponent implements OnInit {
     console.log("formUser.email : ", this.email);
     console.log("formUser.password : ", this.password);
     console.log("formUser.descrizione : ", this.descrizione);
+
+    console.log(this.isValidEmail());
   }
 
   regexJs() {
@@ -45,38 +47,40 @@ export class RegexJsComponent implements OnInit {
     Docs :
     video :yt regex  (https://www.youtube.com/watch?v=3P5wKnrSwqw)
     sito test epressioni regolari : https://regexr.com/
+    sito test epressioni regolari 2 :  https://regex101.com/
+
     */
 
     /*
     // il pattern va indicato all'intero dei due // , esempio /([A-Z])/
-    alla fine e possibile aggiungere dei parametri :
-    1) g : indica global e effettuala la ricerca su tette le corrispondenze che ci sono , se non viene usato si ferma alla prima corrispondenza
-    2) i : indica case insensitive , ovevero la distinzione della stessa lettera tra miauscolo e minuoscolo  e disattiva , se non vienne usato e attiva
-    3) m : indica che la ricerca  , qunado si cerca su fine ella linea come il $ , deve essere fatta su piu linee
+    3) alla fine e possibile aggiungere dei parametri :
+    3.1) g : indica global e effettuala la ricerca su tette le corrispondenze che ci sono , se non viene usato si ferma alla prima corrispondenza
+    3.2) i : indica case insensitive , ovevero la distinzione della stessa lettera tra miauscolo e minuoscolo  e disattiva , se non vienne usato e attiva
+    3.3) m : indica che la ricerca  , qunado si cerca su fine ella linea come il $ , deve essere fatta su piu linee
 
-    meta caratteri :
-    1) . : che non seleziona i cartetti . ma tutti i caratteri e anche gli spazi ad eccezzione dei caratteri di new line
-    2) \w : cerca un caratterrre dalla A alla Z , gli underscor _ , e un numero da 0 a 9
-    3) \W : da il contrario di \w minuscolo , ovvero seleziona tutti i cartatteri che non sono numeri , lettere o underscore.
-    4) \d : cerca un caratterrre numerico da 0 a 9
-    5) \D : da il contrario di \d , ovvero cerca tutti i caratteri che non sono numeri
-    6) \s : seleziona tutti gli spazi
-    7) \S : seleziona I caratteri che non sono spazi e non seleziona i caratteri di nel line
-
-
-    quantificatori :
-    1) ? : indica che quel carattere da cercare e opzionale , cioe deve avere almeno 0 o al massimo 1 occorrenza , mentre il resto della stringa e obbligatoria
-    2) + : indica che quel carattere da cercare deve avere almeno 1 e puo averne piu di una  , mentre il resto della stringa e obbligatoria
-    3) * : indica che quel carattere da cercare deve avere almeno 0 e puo averne piu di una  , mentre il resto della stringa e obbligatoria
-    4) {3} : indica che di quel carattere dobbiamo avere le corrispondenze messe tra le parentesi {} , in questo caso 3 , la corrispondenza e fatta su 3 caratteri esatti
-    5) {3,} : indica che qul carattere deve avere una corripsondenza di almeno 3 caratti o piu di 3
-    5) {3,5} : indica che qul carattere deve avere una corripsondenza di almeno 3 caratti o e al massimo 5 , nel caso e presente piu volte non saranno presi in considerazione i valori oltre il massimo cioe 5
-    6) $: il carattere deve essere alla fine della linea
-    7) ^ : cerca i caratteri ad inizio del testo se combinato con m , sara ad inizio linea
+    4) meta caratteri :
+    4.1) . : che non seleziona i cartetti . ma tutti i caratteri e anche gli spazi ad eccezzione dei caratteri di new line
+    4.2) \w : cerca un caratterrre dalla A alla Z , gli underscor _ , e un numero da 0 a 9
+    4.3) \W : da il contrario di \w minuscolo , ovvero seleziona tutti i cartatteri che non sono numeri , lettere o underscore.
+    4.4) \d : cerca un caratterrre numerico da 0 a 9
+    4.5) \D : da il contrario di \d , ovvero cerca tutti i caratteri che non sono numeri
+    4.6) \s : seleziona tutti gli spazi
+    4.7) \S : seleziona I caratteri che non sono spazi e non seleziona i caratteri di nel line
 
 
-    range e gruppi :
-    1) () : con le parentesi tonde andiamo a creare dei gruppi , che possono essere gruppi numeri (catcging group ) o gruppi non numerati , e al quel gruppo possimao assegnare un quantificatore e il carattere or (|),
+    5) quantificatori :
+    5.1) ? : indica che quel carattere da cercare e opzionale , cioe deve avere almeno 0 o al massimo 1 occorrenza , mentre il resto della stringa e obbligatoria
+    5.2) + : indica che quel carattere da cercare deve avere almeno 1 e puo averne piu di una  , mentre il resto della stringa e obbligatoria
+    5.3) * : indica che quel carattere da cercare deve avere almeno 0 e puo averne piu di una  , mentre il resto della stringa e obbligatoria
+    5.4) {3} : indica che di quel carattere dobbiamo avere le corrispondenze messe tra le parentesi {} , in questo caso 3 , la corrispondenza e fatta su 3 caratteri esatti
+    5.5) {3,} : indica che qul carattere deve avere una corripsondenza di almeno 3 caratti o piu di 3
+    5.6) {3,5} : indica che qul carattere deve avere una corripsondenza di almeno 3 caratti o e al massimo 5 , nel caso e presente piu volte non saranno presi in considerazione i valori oltre il massimo cioe 5
+    5.7) $: il carattere deve essere alla fine della linea
+    5.8) ^ : cerca i caratteri ad inizio del testo se combinato con m , sara ad inizio linea
+
+
+    6) range e gruppi :
+    6.1) () : con le parentesi tonde andiamo a creare dei gruppi , che possono essere gruppi numeri (catcging group ) o gruppi non numerati , e al quel gruppo possimao assegnare un quantificatore e il carattere or (|),
     esempio : /mi piace la (pasta) ? /gi ;
     esempio 2 : mi piace la (pasta | pizza ) /gi ;
     usando le parentesi () andiamo a creare un gruppo , e usando / con la poszione del gruppo possiamo indicare quel gruppo cosa deve cercare , esempio :
@@ -92,14 +96,14 @@ export class RegexJsComponent implements OnInit {
     lo \1 fa riferimento a :  ([a-c]{3}) , perche il primo groppo contiene ?: che non lo fara valere nel coteggio del back refernce (refernce all'indietro )
 
 
-    riferimenti :
+    7) riferimenti :
     in indietro back reference forward reference :
     ad esempio :
-    1) /(io) sono \1 /gi = /io sono io /
-    2) /((io) sono \1) /gi = in questo caso il gruppo esterno e la numerazione precedente o minore del gruppo interno ,
+    7.1) /(io) sono \1 /gi = /io sono io /
+    7.2) /((io) sono \1) /gi = in questo caso il gruppo esterno e la numerazione precedente o minore del gruppo interno ,
     quindi questo e il numero dei gruppi :
-    1) (io sono \1 ) ;
-    2) (io)
+    7.2.1) (io sono \1 ) ;
+    7.2.2) (io)
     quindi per fare riferimento ad io il gruppo diventa ((io) sono \2)
 
     in avanti forward reference :
@@ -108,8 +112,67 @@ export class RegexJsComponent implements OnInit {
     2)(verde)
 
 
+    8) selezione parole o numeri :
+    per selezione i punti come carattere ci basta eseguire l'escape di .  e lo si fa tramite \. ,
+    se usiamo
+    8.1) \d : numeri da 0 a 9
+    8.2) \D : qausiasi carattere tranne i numeri da 0-9
+    8.3) \h : seleziona tutti gli spazi e le tabulazioni
+    8.4) \s : selezionatutti gli spazi le tabulazioni e i ritorni a capo
+    8.5) \w : seleziona tutte le lettere e i numeri da 0-9
+
+    esempio :
+    ([A-Z])\w+ , scomponiamo questa regola :
+    1) prima gruppo ([A-Z]) : e un gruppo con range A-Z seleziona tutte le lettere con lettera maiuscola da a alla z .
+    2) \w : seleziona tutte le lettere e i numeri da 0-9
+    3) + :  indica che quel carattere da cercare deve avere alemno 1 o piu corrispondenze
 
 
+    9) ricerca negli url :
+    ad esempio se ho degli url possiamo provare a cercare
+    9.1) se continene o meno la stringa http o https
+    (http| https)
+    9.2) escape su / :
+    (:\/\/)
+    9.3) inizio url con www. :
+    (w{3}\.)
+    9.4) nome del dominio. con a-z 0-9 e trattini :
+    ([a-z 0-9 ] [a-z 0-9 \-]+ \.) : escape su - , il piu alla fine di range indica che quel range puo avere piu corrispondenze , alla fine aggiunto escape si .
+    9.5) fine del dominio , .it o .com o altro :
+    ([a-z]+) o in altrenativa ([it|com|info])
+
+    espressione compleata : (http| https)?(:\/\/)?(w{3}\.)([a-z 0-9 ][a-z0-9 \-]+\.)([a-z]+)
+    oppure puoi farla cosi: (http| https)?(:\/\/)?(w{3}\.)([a-z]|[0-9]|[\-\.]+)([a-z]+)
+
+
+
+
+    10) word boundaries o limiti di parole :
+    \b : prima della parola cercata deve esserci uno spazio ouna tabulazione e anche dopo
+    \b ciao: prima della parola cercata deve esserci uno spazio ouna tabulazione   , e la c deve essere il primo carattere della parola
+    ciao \b: alla fine  della parola cercata deve esserci uno spazio ouna tabulazione   , e la c deve essere il primo carattere della parola
+    \b ciao \b: prima della parola e alla fine  della parola cercata deve esserci uno spazio ouna tabulazione   , e la c deve essere il primo carattere della parola
+
+
+    11) seleziona pigra o ingorda :
+    11.1) modalita greedy o ingorda :
+    se abbiamo questo testo : ciao a "tutti" questo e un corso sulle "espressioni regolari"
+    e usiamo la seguente regex :\".*" , andra a selezionare da : "tutti " questo e un corso sulle "espressioni regolari" ,
+    questo perche il motore regex de default lavora in modalita greedy ovvero ingorda /avida e provera sempre ad estrapolare la stringa piu lunga possibile
+    11.2) modalita lazy :
+    per attivare la modalita lazy o pigra , che selezionera la stringa piu corta possbile
+
+
+    12) le ancore :
+    le ancore non trovano corrispondenza con alcun carattere il loro scopo e quello di trovare determinate posizioni all'interno del testo , ad esempio :
+    ciao a tutti questo e un corso sulle espressioni regolari :
+    /^[a-z0-9]/gim : prende il primo carattere della stringa per ongi linea
+    /[a-z0-9]$/gim : prende l'ultimo carattere della stringa per ongi linea
+    /\A[a-z0-9]/gim : prende il primo carattere della stringa
+    /[a-z0-9]\Z/gim : prende il primo carattere della stringa , l'unica cosa a cui stare attenti e che se l'ultimo carattere e un ritorno a capo
+    in questo caso trovera sempre corrispondenza , perche \Z considerera come ultimo carattere il carattere precendete
+    /\s\z/gim : seleziona gli spazi le tabulazioni e i ritorni a capo, l'unica cosa a cui stare attenti e che se l'ultimo carattere e un ritorno a capo usiamo \z per
+    prende il ritorno a capo
 
     */
 
@@ -123,6 +186,22 @@ export class RegexJsComponent implements OnInit {
 
   }
 
+
+  isValidEmail(){
+    console.log( "isValidEmail:" ,  this.email)
+    // const regex = /(\w+)@(\w+)\.(\1)/;  // questa regola non funziona perche fopo aver fatto match , il gruppo sara equivalente alla stringa del match meglio definire le regole ogni volta
+    const regex = /(^\w+)@(\w+)\.(\w+)$/;  // questa regola non funziona perche fopo aver fatto match , il gruppo sara equivalente alla stringa del match meglio definire le regole ogni volta
+    // prima regex per verifica dell'email che contega @ e .
+    // uso ^ per far si che il match deve partire dal primo carattere
+    // uso $ perche la stringa deve terminare con il gruppo specificato
+    if(regex.test(this.email.toString())){
+      return true
+    }
+    return false
+  }
+
+
+
   // getter form control
   get nome(): FormControl {
     return this.formUser.get("nome").value as FormControl;
@@ -133,7 +212,7 @@ export class RegexJsComponent implements OnInit {
   }
 
   get email(): FormControl {
-    return this.formUser.get("email").value as FormControl;
+    return this.formUser.get("email").value.toString() as FormControl;
   }
 
   get password(): FormControl {
